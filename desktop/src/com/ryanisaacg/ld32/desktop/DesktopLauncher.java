fb22a91f61300a344a67f249d5df6de90e69deda
Ryan Goldstein <ryanisaacg@gmail.com>
2015-08-24 20:51:48 -0400

    Added name and icons

--- desktop/src/com/ryanisaacg/ld32/desktop/DesktopLauncher.java
+++ desktop/src/com/ryanisaacg/ld32/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.ryanisaacg.ld32.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ryanisaacg.ld33.LD33Game;
@@ -7,9 +8,13 @@ import com.ryanisaacg.ld33.LD33Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Ludum Dare 33";
+		config.title = "Escape From Science";
 		config.width = 1280;
 		config.height = 720;
+		config.vSyncEnabled = true;
+		config.addIcon("icon.png", FileType.Internal);
+		config.addIcon("icon32.png", FileType.Internal);
+		config.addIcon("icon16.png", FileType.Internal);
 		new LwjglApplication(new LD33Game(), config);
 	}
 }
